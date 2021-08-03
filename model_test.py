69e720f08deb0a94331b341d6c0c4db29756787d
Data-Mark 4 <1024935841@qq.com>
2021-08-03 20:12:22 +0800

    mnist datasets pytorch reading

---
+++ model_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import torch
+import torchvision
+
+batch_size = 128
+
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   # torchvision.transforms.Normalize(
+                                   #     (0.1207,), (0.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   # torchvision.transforms.Normalize(
+                                   #     (0.1207,), (0.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=False)
+
+x, y = next(iter(train_loader))
+print(x.shape, y.shape)
